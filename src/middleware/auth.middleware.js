3718c667242852969f0b20618ce1705d82edda72
Hammam Hussein <hamamhussein10@gmail.com>
2026-07-13 21:52:54 +0300

    Middlewares

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import { Errors } from '../utils/map.errors.js';
+import jwt from 'jsonwebtoken';
+import { usersRepository } from '../repository/users.repository.js';
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer '))
+      return next(Errors.unauthorized('Authentication required'));
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
+
+    const user = await usersRepository.getById(decoded.id);
+
+    if (!user) return next(Errors.notFound('User not found'));
+
+    req.user = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
+
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError')
+      return next(Errors.unauthorized('Token expired'));
+
+    if (error.name === 'JsonWebTokenError')
+      return next(Errors.unauthorized('invalid token'));
+
+    next(error);
+  }
+};
---
+++ src/middleware/restricted_to.middleware.js
@@ -0,0 +1,15 @@
+import { Errors } from '../utils/map.errors.js';
+
+export const restrictedTo = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) return next(Errors.unauthorized('Authentication required'));
+
+    const role = req.user.role;
+
+    if (!roles.includes(role))
+      return next(
+        Errors.forbidden('You do not have permission to access this resource'),
+      );
+    next();
+  };
+};
